de58cde0cf76e75ce0961eff0d9ce55ca2210825
Nedim Muhamedagic <muhamedagic.n@gmail.com>
2018-03-31 23:50:22 +0200

    initial home screen component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+// TODO: [@nmuhamedagic]: Add a proper router
+import Home from './routes/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world!</h1>
+        <Home />
       </div>
     );
   }
