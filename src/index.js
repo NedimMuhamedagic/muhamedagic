685e80d8267370bbd5a8586b8538f0cd66fda43e
Nedim Muhamedagic <muhamedagic.n@gmail.com>
2018-03-31 23:46:01 +0200

    initial directory setup and add fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/manifest.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
