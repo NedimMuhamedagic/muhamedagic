b9251e49c4981f86135fa403391e06326929fb0e
Nedim Muhamedagic <muhamedagic.n@gmail.com>
2018-04-01 01:07:32 +0200

    sidebar UI

--- src/components/IntroBox.js
+++ src/components/IntroBox.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { HOME } from 'strings';
 
+import nmImg from 'assets/images/nedimmuhamedagic.jpg';
+
 const IntroBox = () => (
   <div className="IntroBox">
+    <div className="IntroBox__me">
+      <img
+        alt={ HOME.heading }
+        src={ nmImg } />
+    </div>
     <h1 className="IntroBox__heading">
       { HOME.heading }
     </h1>
     <h2 className="IntroBox__subheading">
-      { HOME.subheading }
+      { HOME.subheading }<span className="colorMain"> { HOME.subheadingAccent }</span>
     </h2>
+    <h4>What do I actually do</h4>
+    <ul>
+      {
+        HOME.skills.map( skill => <li key={ skill }>{ skill }</li>)
+      }
+    </ul>
   </div>
 );
 
--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,6 +1,12 @@
 const HOME = {
   heading: 'Nedim Muhamedagić',
-  subheading: 'All things javascript',
+  subheading: 'All things ',
+  subheadingAccent: 'Javascript',
+  skills: [
+    'JS tech lead @AnkoraINC Sarajevo',
+    'React / React Native developer',
+    'Node.js developer',
+  ],
 };
 
 export {
